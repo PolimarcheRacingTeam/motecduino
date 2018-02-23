b7d0746194747d04340668fa3f84f6cb5d8c702b
sebgiles <mrsebgiles@gmail.com>
2018-02-23 21:51:07 +0100

    fixed to work

--- motecduino.ino
+++ motecduino.ino
@@ -1,7 +1,7 @@
 #include "variant.h"
 #include <due_can.h>
 
-#define APP_ID    0x07
+#define APP_ID    0x100
 #define APP_LEN   2
 
 void setup()
@@ -13,20 +13,19 @@ void setup()
   Serial.println("Ready to fool Motec DBW");
 }
 
-uint16_t app = 0; //in raw 12bit DAC counts
+uint16_t app = 15; //in raw 12bit DAC counts
 CAN_FRAME incoming;
 
 void loop() {
-
-  while (!Can0.available()); //wait for packet
-
-  Can0.read(incoming);
-  app = incoming.data.s0;
-
+  
   analogWrite(DAC0, app);  // write the selected waveform on DAC0
   analogWrite(DAC1, app);  // write the selected waveform on DAC1
 
   Serial.print("app = ");
   Serial.println(app);
+
+  while (!Can0.available()); //wait for packet
+  Can0.read(incoming);
+  app = incoming.data.s0;
 
 }
